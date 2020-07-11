c19201621b27ce9fe3dc47c1d000c3044609ac2c
Diego Larrea <diego.larrea@konecta.com.py>
2020-07-11 11:31:50 -0400

    Auto-generated commit

--- src/app/_models/empleo.ts
+++
@@ -1,9 +0,0 @@
-export class Empleo {
-    id: string = null;
-    area: any[] = null;
-    descripcion: string = null;
-    fecha: string = null;
-    imagen: string = null;
-    ubicacion: any[] = null;
-    titulo: string = null;
-}
--- src/app/_models/sitio.ts
+++
@@ -1,6 +0,0 @@
-export class Sitio {
-    id: string = null;
-    nombre: string = null;
-    url: string = null;
-    imagen: string = null;
-}
